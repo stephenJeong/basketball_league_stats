0d6e32d9848fbb5e9f17592f4e5f5c5c84c2adcc
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-03-07 15:07:14 -0800

    WIP - start work on WeeklySchedule component

--- client/src/components/WeeklySchedule.jsx
+++ client/src/components/WeeklySchedule.jsx
@@ -2,8 +2,95 @@ import React from 'react';
 
 function WeeklySchedule ({ props }) {
   return (
-    <div>
-      Weekly Schedule
+    <div className='w-sched'>
+      <div className='w-sched-item w-sched-date'>
+        March 8
+      </div>
+      <div className='w-sched-item matchup'>
+        <div className='mu-time'>
+          3:00PM
+        </div>
+        <div className='mu-home mu-team'>
+          LAL
+        </div>
+        <div className="mu-home-rec mu-team-rec">
+          (45-1)
+        </div>
+        <div className='mu-away mu-team'>
+          BOS
+        </div>
+        <div className='mu-away-rec mu-team-rec'>
+          (1-45)
+        </div>
+      </div>
+      <div className='w-sched-item matchup'>
+        <div className='mu-time'>
+          3:00PM
+        </div>
+        <div className='mu-home mu-team'>
+          LAL
+        </div>
+        <div className="mu-home-rec mu-team-rec">
+          (45-1)
+        </div>
+        <div className='mu-away mu-team'>
+          BOS
+        </div>
+        <div className='mu-away-rec mu-team-rec'>
+          (1-45)
+        </div>
+      </div>
+      <div className='w-sched-item matchup'>
+        <div className='mu-time'>
+          3:00PM
+        </div>
+        <div className='mu-home mu-team'>
+          LAL
+        </div>
+        <div className="mu-home-rec mu-team-rec">
+          (45-1)
+        </div>
+        <div className='mu-away mu-team'>
+          BOS
+        </div>
+        <div className='mu-away-rec mu-team-rec'>
+          (1-45)
+        </div>
+      </div>
+      <div className='w-sched-item matchup'>
+        <div className='mu-time'>
+          3:00PM
+        </div>
+        <div className='mu-home mu-team'>
+          LAL
+        </div>
+        <div className="mu-home-rec mu-team-rec">
+          (45-1)
+        </div>
+        <div className='mu-away mu-team'>
+          BOS
+        </div>
+        <div className='mu-away-rec mu-team-rec'>
+          (1-45)
+        </div>
+      </div>
+      <div className='w-sched-item matchup'>
+        <div className='mu-time'>
+          3:00PM
+        </div>
+        <div className='mu-home mu-team'>
+          LAL
+        </div>
+        <div className="mu-home-rec mu-team-rec">
+          (45-1)
+        </div>
+        <div className='mu-away mu-team'>
+          BOS
+        </div>
+        <div className='mu-away-rec mu-team-rec'>
+          (1-45)
+        </div>
+      </div>
     </div>
   )
 };
