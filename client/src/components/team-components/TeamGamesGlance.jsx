1a647420c67a264e8d79ffe7218ca69161821551
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-03-14 13:12:14 -0700

    add import files for team components

--- client/src/components/team-components/TeamGamesGlance.jsx
+++ client/src/components/team-components/TeamGamesGlance.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TeamGame from './TeamGame.jsx';
 
 const TeamGamesGlance = ({ listPropsHere }) => {
   return (
---
+++ client/src/components/team-components/TeamPlayersView.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TeamPlayers from './TeamPlayers.jsx';
+
+const TeamPlayersView = ({ listPropsHere }) => {
+  return (
+    <div>
+      Team Players View
+    </div>
+  )
+};
+
+export default TeamPlayersView;
