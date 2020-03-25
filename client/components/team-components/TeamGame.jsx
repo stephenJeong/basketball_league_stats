cc8e79928e30050761716975d202e06ff4965abc
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-03-24 19:39:10 -0700

    finish games at a glance component

--- client/components/team-components/TeamGame.jsx
+++ client/components/team-components/TeamGame.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 
-const TeamGame = ({ week, date }) => {
+const TeamGame = ({ week }) => {
   return (
     <div id="gg-game">
       <div id="gg-game-date">
         {/* {week} {date} */}
-        <strong>LAST GAME</strong> Sunday, March 1
+        <strong>{week.week}</strong> {week.date}
       </div>
       <div id="gg-game-sched">
         <div>
-          <img src="https://www.stickpng.com/assets/images/58419d0aa6515b1e0ad75a6c.png" alt="lakers" width="150" /><br/>
+          <img src="https://www.stickpng.com/assets/images/58419d0aa6515b1e0ad75a6c.png" alt="lakers" width="60" /><br/>
           72-5
         </div>
-        <div>
+        <div className="gg-score">
           110
         </div>
-        <div>
+        <div className="gg-status">
           FINAL
         </div>
-        <div>
+        <div className="gg-score">
           50
         </div>
         <div>
-          <img src="https://www.stickpng.com/assets/images/58419c6aa6515b1e0ad75a61.png" alt="celtics" width="150" /><br/>
+          <img src="https://www.stickpng.com/assets/images/58419c6aa6515b1e0ad75a61.png" alt="celtics" width="60" /><br/>
           5-72
         </div>
       </div>
--- client/components/team-components/TeamGamesGlance.jsx
+++ client/components/team-components/TeamGamesGlance.jsx
@@ -2,10 +2,27 @@ import React from 'react';
 import TeamGame from './TeamGame.jsx';
 
 const TeamGamesGlance = ({ listPropsHere }) => {
+  let lastWeek = {
+    week: "LAST GAME",
+    date: "Sunday, March 1"
+  };
+
+  let nextWeek = {
+    week: "NEXT GAME",
+    date: "Sunday, March 8"
+  };
+
   return (
     <div id="games-glance">
-      Games at a Glance
-      <TeamGame />
+      <div id="gg-title">
+        <h2>Games at a Glance</h2>
+      </div>
+      <div>
+        <TeamGame week={lastWeek} />
+      </div>
+      <div>
+        <TeamGame week={nextWeek} />
+      </div>
     </div>
   )
 };
