2883fb8a396796be40d6d5a189cd5aa733bc22c1
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-04-06 18:49:07 -0700

    add styling to router Links

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -6,14 +6,20 @@ const NavBar = ({ props }) => {
   return (
     <div className="navBar">
       <div className="navBar-item">
-        <Link to="/">Home</Link>
+        <Link to="/" className="navBar-links">Home</Link>
       </div>
-      <div className="navBar-item">Schedule</div>
       <div className="navBar-item">
-        <Link to="/teams">Teams</Link>
+        <Link to="/" className="navBar-links">Schedule</Link>
       </div>
-      <div className="navBar-item">Standings</div>
-      <div className="navBar-item">Stats</div>
+      <div className="navBar-item">
+        <Link to="/teams" className="navBar-links">Teams</Link>
+      </div>
+      <div className="navBar-item">
+        <Link to="/" className="navBar-links">Standings</Link>
+      </div>
+      <div className="navBar-item">
+          <Link to="/" className="navBar-links">Stats</Link>
+      </div>
       <div className="navBar-item"></div>
     </div>
   )
