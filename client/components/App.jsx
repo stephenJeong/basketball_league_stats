4493a222e12e2dcb9fe130d244e4e1cd1e8af941
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-04-11 18:17:44 -0700

    WIP - getting data from express to react

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
+import axios from 'axios';
 import { render } from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './Landing.jsx';
 import TeamsView from './team-components/TeamsView.jsx';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component={Landing} />
-        <Route path='/teams' component={TeamsView} />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.testGet = this.testGet.bind(this);
+  }
+
+  testGet() {
+    axios.get('/api')
+      .then((res) => {
+        console.log(`res from testGet ${res}`)
+      })
+      .catch((err) => {
+        console.log(`error while getting data: ${err}`);
+      })
+  }
+
+  componentDidMount() {
+    this.testGet();
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route path='/teams' component={TeamsView} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 };
 
 render(<App />, document.getElementById('app'));
