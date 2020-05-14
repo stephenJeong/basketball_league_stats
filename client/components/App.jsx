3929b6ac2dae4da3e36b3f17d8ed37484c3cd540
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-05-14 15:04:03 -0700

    Remove commented out code

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -14,26 +14,34 @@ class App extends React.Component {
       playerStats: [],
       leaders: [],
       teamStats: [],
-      schedule: [],
+      allSchedule: [],
+      sortedTeams: [],
+      selectedTeam: {},
+      teamSchedule: {
+        lastGame: {},
+        nextGame: {},
+      },
     };
 
     this.getPlayerStats = this.getPlayerStats.bind(this);
     this.reverseSortPlayers = this.reverseSortPlayers.bind(this);
     this.getTeamStats = this.getTeamStats.bind(this);
     this.getSchedule = this.getSchedule.bind(this);
+    this.setDefaultTeam = this.setDefaultTeam.bind(this);
   }
 
   componentDidMount() {
+    this.getSchedule();
     this.getPlayerStats();
     this.getTeamStats();
-    this.getSchedule();
+    this.setDefaultTeam();
   }
 
   getPlayerStats() {
     axios.get('/api/player')
       .then((res) => {
         const sortedPlayers = this.reverseSortPlayers(res.data);
-        const leaders = sortedPlayers.splice(0, 3);
+        const leaders = sortedPlayers.splice(0, 5);
         this.setState({
           playerStats: sortedPlayers,
           leaders: leaders,
@@ -48,7 +56,7 @@ class App extends React.Component {
     axios.get('/api/schedule')
       .then((res) => {
         this.setState({
-          schedule: res.data,
+          allSchedule: res.data,
         });
       })
       .catch((err) => {
@@ -86,14 +94,74 @@ class App extends React.Component {
         this.setState({
           teamStats: res.data,
         });
+
+
       })
       .catch((err) => {
         console.log(`error while getting team data: ${err}`);
       });
   }
 
+  setDefaultTeam() {
+    // only send unique teams
+    let uniqueTeams = [];
+    this.state.teamStats.forEach((elem) => {
+      if (!uniqueTeams.includes(elem.name)) {
+        uniqueTeams.push(elem.name);
+      }
+    });
+
+    // sort teams in alphabetical order
+    let sort = uniqueTeams.sort((a, b) => {
+      if (a < b) { return -1; }
+      if (a > b) { return 1; }
+      return 0;
+    });
+
+    // set default team
+    let team = {};
+
+    uniqueTeams.forEach((elem) => {
+      if (elem.name === sort[0]) {
+        team = elem;
+      }
+    });
+
+    // find last week and next week Sunday's dates
+    let today = new Date();
+    let yyyy = today.getFullYear();
+    let mm = String(today.getMonth() + 1);
+    let ddLast = String(today.getDate() + (0 - today.getDay() % 7));
+    let ddNext = String(today.getDate() + (7 - today.getDay() % 7));
+
+    let lastSunday = mm + '/' + ddLast + '/' + yyyy;
+    let nextSunday = mm + '/' + ddNext + '/' + yyyy;
+
+    // separate out schedule for selected team
+    let teamSchedule = {}
+    this.state.allSchedule.forEach((elem) => {
+      if (elem.awayTeam === team.name || elem.homeTeam === team.name) {
+        if (elem.date === lastSunday) {
+          teamSchedule.lastWeek = elem;
+        } else if (elem.date === nextSunday) {
+          teamSchedule.nextWeek = elem;
+        }
+      }
+    });
+
+    console.log('teamSchedule in app.jsx:', teamSchedule)
+
+    this.setState({
+      sortedTeams: sort,
+      selectedTeam: team,
+      teamSchedule: teamSchedule,
+    });
+  }
+
+
+
   render() {
-    let { playerStats, leaders, teamStats, schedule } = this.state;
+    let { playerStats, leaders, teamStats, sortedTeams, selectedTeam, teamSchedule } = this.state;
 
     return (
       <div>
@@ -102,7 +170,10 @@ class App extends React.Component {
             <Navbar />
             <Switch>
               <Route exact path="/" render={(routeProps) => (<Landing {...routeProps} playerStats={playerStats} leaders={leaders} />)} />
-              <Route path="/teams" render={(routeProps) => (<TeamsView {...routeProps} teamStats={teamStats} playerStats={playerStats} schedule={schedule} />)} />
+              <Route path="/teams" render={(routeProps) => (
+                <TeamsView {...routeProps} teamStats={teamStats} playerStats={playerStats} sortedTeams={sortedTeams} selectedTeam={selectedTeam} schedule={teamSchedule} />
+                )}
+              />
             </Switch>
           </div>
         </BrowserRouter>
