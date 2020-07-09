fca39853ff4ed2f71e6b459a8745a5a5eb7cd09d
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-07-08 22:17:40 -0700

    update random picture generator

--- client/components/Announcements.jsx
+++ client/components/Announcements.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Announcements = () => (
   <div>
-    <div><img className="hero-img" src="https://i.picsum.photos/id/911/600/400.jpg" alt="announcement" /></div>
+    <div><img className="hero-img" src="http://lorempixel.com/600/400/" alt="announcement" /></div>
     {/* How to do overlay for announcments like nba.com */}
   </div>
 );
