84fcad7a2cc1ebd4dcb9c0e1ee0bfbe1272af585
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-12-22 18:00:26 -0800

    fix path on proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    proxy('/.netlify/functions/server/api', {
+    proxy('/.netlify/functions/index/api', {
       target: 'http://localhost:9000/',
     }),
   )};
